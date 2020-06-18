e8a717906a55b39b1174134158cfe0632518f90b
Frizlab <fload@me.com>
2020-06-18 11:18:50 +0200

    Initial Commit

---
+++ Memory Eater/ContentView.swift
@@ -0,0 +1,28 @@
+/*
+ * ContentView.swift
+ * Memory Eater
+ *
+ * Created by François Lamboley on 18/06/2020.
+ * Copyright © 2020 Frizlab. All rights reserved.
+ */
+
+import SwiftUI
+
+
+
+struct ContentView: View {
+	
+	var body: some View {
+		Text("Hello, World!")
+	}
+	
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+	
+	static var previews: some View {
+		ContentView()
+	}
+	
+}
